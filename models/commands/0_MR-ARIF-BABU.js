f9c844cf404d960caa48ec8242cdec2796b90202
PRINCE MEGHWANSI <princemeghwansi1@gmail.com>
2025-01-03 09:46:25 +0530

    Update 0_MR-ARIF-BABU.js

--- models/commands/0_MR-ARIF-BABU.js
+++ models/commands/0_MR-ARIF-BABU.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "1.0.0",
   hasPermssion: 0,
   credits: "ARIF BABU",
-  description: "Arif dp",
+  description: "Prince dp",
   usePrefix: true,
   commandCategory: "Random-IMG",
   usages: "arif babu",
@@ -21,8 +21,8 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://i.imgur.com/A1Qift2.jpg","https://i.imgur.com/8WUF7HJ.jpg","https://i.imgur.com/mLJw7tc.jpg","https://i.imgur.com/a1NPl9F.jpg","https://i.imgur.com/nBYDoNT.jpg","https://i.imgur.com/coGeSau.jpg","https://i.imgur.com/2JjkK0l.jpg","https://i.imgur.com/7qIldX1.jpg","https://i.imgur.com/YpDPpq5.jpg","https://i.imgur.com/c1aaVpd.jpg","https://i.imgur.com/U4eQKKS.jpg","https://i.imgur.com/R9W8CgK.jpg","https://i.imgur.com/xXGtR1H.jpg","https://i.imgur.com/4l2CyaF.jpg","https://i.imgur.com/GZyFV0o.jpg",
+"https://i.imgur.com/8WUF7HJ.jpg","https://i.imgur.com/8WUF7HJ.jpg","https://i.imgur.com/mLJw7tc.jpg","https://i.imgur.com/a1NPl9F.jpg","https://i.imgur.com/nBYDoNT.jpg","https://i.imgur.com/coGeSau.jpg","https://i.imgur.com/2JjkK0l.jpg","https://i.imgur.com/7qIldX1.jpg","https://i.imgur.com/YpDPpq5.jpg","https://i.imgur.com/c1aaVpd.jpg","https://i.imgur.com/U4eQKKS.jpg","https://i.imgur.com/R9W8CgK.jpg","https://i.imgur.com/xXGtR1H.jpg","https://i.imgur.com/4l2CyaF.jpg","https://i.imgur.com/GZyFV0o.jpg",
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     ARIF-BABU                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nDEKH LO MARA BOSS ARIF BABU KO SABSE ALAG MASOOM SA CHEHRA BHOLE SE SURAT AANKH ME PYAR DIL ME BUKHAR ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     PRINCE MEGHWANSI                   ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nDEKH LO MARA BOSS PRINCE BABU KO SABSE ALAG MASOOM SA CHEHRA BHOLE SE SURAT AANKH ME PYAR DIL ME BUKHAR ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    }
