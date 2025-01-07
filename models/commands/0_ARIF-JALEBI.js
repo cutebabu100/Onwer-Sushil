0b7179ef9aaeffb6504b66a4ee84810a2a8e0da9
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-07 12:14:18 +0530

    Update 0_ARIF-JALEBI.js

--- models/commands/0_ARIF-JALEBI.js
+++ models/commands/0_ARIF-JALEBI.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 		var msg = {
 				body: "💝 YE LO BBY JALEBI KHAO 🥨\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝐏𝐫𝐢𝐧𝐜𝐞❤️-)",
 				attachment: 
-fs.createReadStream(__dirname + `/cache/Jalebi.jpg`)
+fs.createReadStream(__dirname + `/ARIF-BABU/JALEBI.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥨", event.messageID, (err) => {}, true)
