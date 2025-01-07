748b62c81998bef35ead09da241789fc307bb639
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-07 12:11:33 +0530

    Update 0_ARIF-MAGGIE.js

--- models/commands/0_ARIF-MAGGIE.js
+++ models/commands/0_ARIF-MAGGIE.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 		var msg = {
 				body: "💝 YE LO BBY MAGGIE KHAO 🥗\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝐏𝐫𝐢𝐧𝐜𝐞❤️-)",
 				attachment: 
-fs.createReadStream(__dirname + `/cache/Maggie.jpg`)
+fs.createReadStream(__dirname + `/ARIF-BABU/MAGGIE.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥗", event.messageID, (err) => {}, true)
