84135e3606569a5710aa692de9288b941efcb4a2
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-08 18:35:03 +0530

    Update 0_ARIF-KURKURE.js

--- models/commands/0_ARIF-KURKURE.js
+++ models/commands/0_ARIF-KURKURE.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 		var msg = {
 				body: "💝 YE LO BBY KURKURE KHAO 🫓\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝐏𝐫𝐢𝐧𝐜𝐞❤️-)",
 				attachment: 
-fs.createReadStream(__dirname + `/cache/KURKURE.jpg`)
+fs.createReadStream(__dirname + `/ARIF-BABU/KURKURE.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫓", event.messageID, (err) => {}, true)
