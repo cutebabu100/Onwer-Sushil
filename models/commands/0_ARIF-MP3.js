1673afaebd57544eda9c83c140b74fc7a10fd522
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-08 10:27:20 +0530

    Update 0_ARIF-MP3.js

--- models/commands/0_ARIF-MP3.js
+++ models/commands/0_ARIF-MP3.js
@@ -12,10 +12,10 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("Arif")==0 || event.body.indexOf("arif")==0 || event.body.indexOf("Arif babu")==0 || event.body.indexOf("ARIF")==0) {
+  if (event.body.indexOf("Arif")==0 || event.body.indexOf("arif")==0 || event.body.indexOf("Prince babu")==0 || event.body.indexOf("PRINCE")==0) {
     var msg = {
-        body: "मुझे मेरे आरिफ बाबू बॉस ने बनाया है 😐",
-        attachment: fs.createReadStream(__dirname + `/ARIF-BABU/dk.mp3`)
+        body: "मुझे मेरे प्रिंस बाबू बॉस ने बनाया है 😐",
+        attachment: fs.createReadStream(__dirname + `/PRINCE-BABU/dk.mp3`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
