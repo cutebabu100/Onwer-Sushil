ca54e16b2bfe3ea91778be1871770e4fb4415651
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-25 21:10:54 +0530

    Update 0_ARIF-PAIR-2.js

--- models/commands/0_ARIF-PAIR-2.js
+++ models/commands/0_ARIF-PAIR-2.js
@@ -18,7 +18,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'aar2.jpg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/JZX0BI1.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/DDH7Jz9/image.jpg", path);
 }
 async function makeImage({ one, two }) {
     const fs = global.nodemodule["fs-extra"];
