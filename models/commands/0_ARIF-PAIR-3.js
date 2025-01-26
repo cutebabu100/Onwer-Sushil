3bbf79833a3ba5f97e91eb4a8d4c81d11ad1046d
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-01-26 15:16:57 +0530

    Update 0_ARIF-PAIR-3.js

--- models/commands/0_ARIF-PAIR-3.js
+++ models/commands/0_ARIF-PAIR-3.js
@@ -2,10 +2,10 @@ module.exports.config = {
   name: "pair3",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "ARIF BABU",
+  credits: "faiz Ansari",
   description: "It's a compound :>",
   commandCategory: "Giải trí",
-  usages: "",
+  usages: "[@mention]",
   dependencies: {
         "axios": "",
         "fs-extra": ""
@@ -20,7 +20,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let pathImg = __dirname + "/cache/background.png";
   let pathAvt1 = __dirname + "/cache/Avtmot.png";
   let pathAvt2 = __dirname + "/cache/Avthai.png";
-
+  
   var id1 = event.senderID;
   var name1 = await Users.getNameUser(id1);
   var ThreadInfo = await api.getThreadInfo(event.threadID);
@@ -55,14 +55,14 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
   var rd2 = cc[Math.floor(Math.random() * cc.length)];
   var djtme = [`${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd2}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`];
-
+  
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.imgur.com/mHAHENR.jpg",
+  "https://i.imgur.com/CYsOXjr.jpg",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
-
+  
   let getAvtmot = (
     await axios.get(
       `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
@@ -98,7 +98,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
   fs.removeSync(pathAvt2);
-  return api.sendMessage({ body: `🍫${name1}🍫\n🍫${name2}🍫\n✧═══•❁『${tile}%』❁•═══✧`,
+  return api.sendMessage({ body: `🍫${name1}🍫\nLOVE🍫${name2}🍫\n✧═══•❁『${tile}%』❁•═══✧`,
             mentions: [{
           tag: `${name2}`,
           id: id2
@@ -106,4 +106,4 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
       event.threadID,
       () => fs.unlinkSync(pathImg),
       event.messageID);
-  }
+}
