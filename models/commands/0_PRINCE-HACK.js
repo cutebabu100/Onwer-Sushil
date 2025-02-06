e81644a52bbc3cdbec86a2ede3ffafc7b8827d89
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-02-06 20:52:11 +0530

    Update 0_PRINCE-HACK.js

--- models/commands/0_PRINCE-HACK.js
+++ models/commands/0_PRINCE-HACK.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   
   var background = [
 
-    "https://i.imgur.com/VQXViKI.png"
+    "https://i.ibb.co/ch4DnkKG/image.jpg"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
   
