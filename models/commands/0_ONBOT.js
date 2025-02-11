275cfacefda839d4e638855f510b76ce68a011ad
Ishika876 <158852722+Ishika876@users.noreply.github.com>
2025-02-11 15:08:16 +0530

    Update 0_ONBOT.js

--- models/commands/0_ONBOT.js
+++ models/commands/0_ONBOT.js
@@ -1,4 +1,3 @@
-
 module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
@@ -9,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100029323637418", "100029323637418"];
+    const permission = ["100080952386176", "100080287361043"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For FAIZ ANSARI", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.enter(0))
 }
