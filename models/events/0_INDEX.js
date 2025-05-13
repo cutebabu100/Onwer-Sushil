02a5dc8dd5535752ed9eba8007d73843f9ccbc77
anil28037 <anilrajput89470750@gmail.com>
2025-05-13 12:13:40 +0530

    Update 0_INDEX.js

--- models/events/0_INDEX.js
+++ models/events/0_INDEX.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100055039334431";
+  var god = "100092324977131";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
