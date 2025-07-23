a106ada2fd3519567cc7d07b21ba1b32edb1777e
anil28037 <anilrajput89470750@gmail.com>
2025-07-23 10:20:35 +0530

    Update 0_ARIF-PAIR-3.js

--- models/commands/0_ARIF-PAIR-3.js
+++ models/commands/0_ARIF-PAIR-3.js
@@ -59,7 +59,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.imgur.com/CYsOXjr.jpg",
+  "https://i.ibb.co/HTS2jcJL/CYsOXjr.jpg",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
   
