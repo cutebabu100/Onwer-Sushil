1aaa4e84b8631e58ac8ba7da7d921ca7bcf77f8e
SUSH1L KUM9R Y9D9V 😌🔐❤️ <sushiilkumar103@gmail.com>
2025-08-02 11:47:43 +0500

    Update 0_ARIF-KI-DP-.js

--- models/commands/0_ARIF-KI-DP-.js
+++ models/commands/0_ARIF-KI-DP-.js
@@ -14,11 +14,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
   if(react.includes("prince") ||
-     react.includes("prince") || react.includes("prince") || react.includes("@𓏵𓋠𒅓𓏪𓉚𝐏𝐑𝐈𝐍𝐂𝐄𒀭𝐁𝐀𝐁𝐔𓉛𓏪𒅓𓋠𓏵 𒆜𒋟❮𝐑𝐀𝐉𝐀𝐒𝐓𝐀𝐍𒀭𝗞𝗔𒀭𝗕𝗔𝗗𝗦𝗛𝗔𝗛❯𒋟𒆜 𒀭𒀮𓁫𓂔𓏵𓏜𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞𓏜𓏵𓂔𓁬𒀮𒀭 𓃉𓃊𓃑𓄋𝐈𝐓𝐒𓇻︎𓇻𝐁𝐑𝐀𝐍𝐃𓄋𓃑𓃊𓃉 𓆈 y -  【‿】") ||
-react.includes("prince babu") ||
-react.includes("prince meghwansi")) {
+     react.includes("prince") || react.includes("prince") || react.includes("@𓏵𓋠𒅓𓏪𓉚𝐒𝐔𝐒𝐇𝐈𝐋𒀭𝐁𝐀𝐁𝐔𓉛𓏪𒅓𓋠𓏵 𒆜𒋟❮𝐔𝐓𝐓𝐀𝐑 𝐏𝐑𝐀𝐃𝐄𝐒𝐇𒀭𝗞𝗔𒀭𝗕𝗔𝗗𝗦𝗛𝗔𝗛❯𒋟𒆜 𒀭𒀮𓁫𓂔𓏵𓏜𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞𓏜𓏵𓂔𓁬𒀮𒀭 𓃉𓃊𓃑𓄋𝐈𝐓𝐒𓇻︎𓇻𝐁𝐑𝐀𝐍𝐃𓄋𓃑𓃊𓃉 𓆈 y -  【‿】") ||
+react.includes("sushil") ||
+react.includes("sushil kumar")) {
     var msg = {
-        body: `𝐃𝐄𝐊𝐇𝐎 𝐌𝐄 𝐀 𝐆𝐀𝐘𝐀 𝐀𝐀𝐏𝐊𝐀 𝐁𝐎𝐒𝐒 𝐏𝐑𝐈𝐍𝐂𝐄 𝐌𝐄𝐆𝐇𝐖𝐀𝐍𝐒𝐈 🙈 😎 𓆩♡𓆪`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/PRINCE.mp4`)
+        body: `𝐃𝐄𝐊𝐇𝐎 𝐌𝐄 𝐀 𝐆𝐀𝐘𝐀 𝐀𝐀𝐏𝐊𝐀 𝐁𝐎𝐒𝐒 𝐒𝐔𝐒𝐇𝐈𝐋 𝐘𝐀𝐃𝐀𝐕 🙈 😎 𓆩♡𓆪`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/PRINCE.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
